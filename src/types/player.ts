ef5eea4344947719a8431df8529291249f9101d6
Zoniaina <fzoniaina@gmail.com>
2023-11-08 13:49:47 +0300

    add zod type

---
+++ src/types/player.ts
@@ -0,0 +1,30 @@
+import { z } from 'zod';
+
+export const PlayerSchema = z.object({
+  id: z.number(),
+  name: z.string(),
+  score: z.number(),
+  dice: z.array(z.number()),
+  winner: z.boolean().default(false),
+});
+
+export type Player = z.infer<typeof PlayerSchema>;
+
+const PLayerInputSchema = PlayerSchema.pick({ name: true });
+
+export type PlayerInput = z.infer<typeof PLayerInputSchema>;
+
+export const GameContextSchema = z.object({
+  players: z.array(PlayerSchema),
+  addPlayer: z.function().args(z.string()).returns(z.void()),
+  rollDice: z.function().args(z.number()).returns(z.void()),
+  currentTurn: z.number(),
+  nextTurn: z.function().returns(z.void()),
+  reinitScores: z.function().returns(z.void()),
+  initGame: z.function().returns(z.void()),
+  dice1: z.number().default(1),
+  dice2: z.number().default(1),
+  winner: PlayerSchema.nullable(),
+});
+
+export type GameContextType = z.infer<typeof GameContextSchema>;
